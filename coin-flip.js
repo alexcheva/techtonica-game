9dcc84104ab6a267e331d26ceac0924ebc286324
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-02-08 14:25:19 -0800

    expand logic, add promt and alert

--- coin-flip.js
+++ coin-flip.js
@@ -4,13 +4,22 @@
 // Extensions:
 
 // Add a box where the user can say how many coins they want to flip. Then display that many coins and flip them all randomly.
-
+var num = window.prompt("How many coins would you like to flip?");
+console.log(num, typeof num);
+console.log(parseInt(num),typeof parseInt(num))
 function flipCoin(num){
   let output = ["heads","tails"];
-  for(let i=1; i<=num; i++){
-    let result = Math.floor(Math.random()*2);
-    console.log(result);
-    console.log(`Coin ${i} lands on ${output[result]}.`)
+  num = parseInt(num);
+  if(isNaN(num)){
+    window.alert("Please, choose a numeric value.")
+  }else{
+    
+    for(let i=1; i<=num; i++){
+      let result = Math.floor(Math.random()*2);
+      console.log(result);
+      console.log(`Coin ${i} lands on ${output[result]}.`)
+    }
   }
+  
 }
-flipCoin(5);
+flipCoin(num);
